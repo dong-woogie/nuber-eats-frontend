6d70d320a2ee4038b0a70b5c0d9b1513f13b831a
dong-woogie <com6511@gmail.com>
2020-12-31 20:14:52 +0900

    setup : apllo client

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,19 @@
+import { ApolloProvider } from "@apollo/client";
+import { BrowserRouter } from "react-router-dom";
 import React from "react";
 import ReactDOM from "react-dom";
+import { client } from "./apollo";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "./styles/styles.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ApolloProvider>,
+  document.getElementById("root")
+);
 
 reportWebVitals();
