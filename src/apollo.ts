74aab3743203d43893a632fe077e53eadc363c8d
dong-woogie <com6511@gmail.com>
2021-01-04 00:23:31 +0900

    Customizing request logic

--- src/apollo.ts
+++ src/apollo.ts
@@ -1,12 +1,32 @@
-import { ApolloClient, InMemoryCache, makeVar } from "@apollo/client";
+import {
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache,
+  makeVar,
+  ApolloLink,
+  concat,
+} from "@apollo/client";
 import { LOCAL_STORAGE_TOKEN } from "./constants";
 
 const token = localStorage.getItem(LOCAL_STORAGE_TOKEN);
 export const loggedVars = makeVar(Boolean(token));
 export const authTokenVars = makeVar(token);
 
-export const client = new ApolloClient({
+const httpLink = createHttpLink({
   uri: "http://localhost:4000/graphql",
+});
+
+const authMiddleware = new ApolloLink((operation, forward) => {
+  operation.setContext({
+    headers: {
+      "x-jwt": authTokenVars(),
+    },
+  });
+  return forward(operation);
+});
+
+export const client = new ApolloClient({
+  link: concat(authMiddleware, httpLink),
   cache: new InMemoryCache({
     typePolicies: {
       Query: {
