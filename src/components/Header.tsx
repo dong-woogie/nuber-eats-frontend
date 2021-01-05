bf97d6be36ad5594d99200a70c954d32208b3fd1
dong-woogie <com6511@gmail.com>
2021-01-06 01:44:55 +0900

    Implement : header components style

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,14 +2,11 @@ import React from "react";
 import Logo from "../images/nuber-logo.svg";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Link } from "react-router-dom";
-import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import { useMe } from "../hooks/useMe";
-import { useReactiveVar } from "@apollo/client";
-import { loggedVars } from "../apollo";
 
 function Header() {
   const { data } = useMe();
-  const isLogged = useReactiveVar(loggedVars);
   return (
     <>
       {data && !data?.me.verified && (
@@ -19,20 +16,30 @@ function Header() {
           </span>
         </div>
       )}
-      <header className="py-10">
-        <div className="w-full px-8 xl:px-0 max-w-screen-xl mx-auto flex justify-between items-center">
+      <header className="py-5">
+        <div className="w-full px-8 xl:px-0 max-w-screen-xl mx-auto flex flex-wrap justify-between items-center">
           <Link to="/">
             <img src={Logo} alt="logo" className="w-40" />
           </Link>
-          <span>
-            {isLogged ? (
-              <Link to="/edit-profile">
-                <FontAwesomeIcon icon={faUser} className="text-2xl" />
-              </Link>
-            ) : (
-              <Link to="/login">로그인</Link>
-            )}
-          </span>
+          <div className="hidden sm:block w-1/2">
+            <form className="relative w-full">
+              <FontAwesomeIcon
+                icon={faSearch}
+                className="absolute text-xl left-5 top-5"
+              />
+              <input
+                type="Search"
+                className="search"
+                placeholder="what are you craving?"
+              />
+            </form>
+          </div>
+          <div className="sm:hidden w-full flex items-center justify-between pt-5">
+            <div></div>
+            <div className="w-10 h-10 rounded-full cursor-pointer bg-gray-200 hover:bg-gray-300 active:bg-gray-400 flex justify-center items-center">
+              <FontAwesomeIcon icon={faSearch} className="text-lg" />
+            </div>
+          </div>
         </div>
       </header>
     </>
