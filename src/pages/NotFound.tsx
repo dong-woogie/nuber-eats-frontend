204084d1cb0abbfebd1e5d5c6405ee02300cf113
dong-woogie <com6511@gmail.com>
2021-01-12 00:15:46 +0900

    testcode : NotFound page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,9 +1,13 @@
 import React from "react";
+import { Helmet } from "react-helmet-async";
 import { Link } from "react-router-dom";
 
 function NotFound() {
   return (
     <div className="mt-52 flex flex-col items-center justify-center">
+      <Helmet>
+        <title>Not found | Nuber Eats</title>
+      </Helmet>
       <h1 className="title">Not Found Page 404</h1>
       <h4 className="sub text-center mx-5 sm:mx-0">
         The page you're looking for does not exist or has moved.
