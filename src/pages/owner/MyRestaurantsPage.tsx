16938d1fffd304ecb5571c992e0b35609e6c8afb
dong-woogie <com6511@gmail.com>
2021-01-13 00:01:23 +0900

    Role이 Owner일 때 메인페이지 추가

---
+++ src/pages/owner/MyRestaurantsPage.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Helmet } from "react-helmet-async";
+
+function MyRestaurantsPage() {
+  return (
+    <div>
+      <Helmet>
+        <title>Owner Home | Nuber Eats</title>
+      </Helmet>
+      My Restaurants
+    </div>
+  );
+}
+
+export default MyRestaurantsPage;
