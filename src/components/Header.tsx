65e94f3d9273e0ec2d08c732f15053e9f7f2f909
dong-woogie <com6511@gmail.com>
2021-01-13 15:18:35 +0900

    refactoring : Header component UI

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import Logo from "../images/nuber-logo.svg";
 import { Link } from "react-router-dom";
 import { useMe } from "../lib/hooks/useMe";
 import SearchForm from "./SearchForm";
-import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { faSearch, faUser } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function Header() {
   const { data } = useMe();
+  const [isSearchForm, setIsSearchForm] = useState(false);
+
+  const onClickSearchIcon = () => {
+    setIsSearchForm(!isSearchForm);
+  };
+
   return (
     <>
       {data && !data?.me.verified && (
@@ -22,21 +28,32 @@ function Header() {
           <Link to="/" className="order-1">
             <img src={Logo} alt="logo" className="w-40" />
           </Link>
-          <div className="w-full sm:w-2/5 mt-10 sm:mt-0 flex items-center order-3 sm:order-2 sm:ml-auto">
-            <SearchForm />
+
+          <div
+            className={`w-10 h-10 rounded-full cursor-pointer order-2 sm:order-3 ${
+              isSearchForm ? "bg-gray-300" : "bg-white"
+            } flex justify-center items-center ml-auto ${
+              isSearchForm && "sm:ml-5"
+            }`}
+            onClick={onClickSearchIcon}
+          >
+            <FontAwesomeIcon icon={faSearch} className="text-lg" />
           </div>
+
           <Link
             to="/edit-profile"
-            className="w-14 h-14 flex justify-center items-center order-2 sm:order-3 ml-auto sm:ml-5"
+            className="w-14 h-14 flex justify-center items-center order-3 sm:order-4"
           >
             <FontAwesomeIcon icon={faUser} className="text-xl" />
           </Link>
-          {/* <div className="sm:hidden w-full flex items-center justify-between pt-5">
-            <div></div>
-            <div className="w-10 h-10 rounded-full cursor-pointer bg-gray-200 hover:bg-gray-300 active:bg-gray-400 flex justify-center items-center">
-              <FontAwesomeIcon icon={faSearch} className="text-lg" />
-            </div>
-          </div> */}
+
+          <div
+            className={`${
+              isSearchForm ? "block" : "hidden"
+            } w-full sm:w-2/5 mt-3 sm:mt-0 flex items-center order-4 sm:order-2 sm:ml-auto `}
+          >
+            <SearchForm />
+          </div>
         </div>
       </header>
     </>
