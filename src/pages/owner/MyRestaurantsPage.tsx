2820ccc611b9fbf9c024aa5ac14356e1145925fd
dong-woogie <com6511@gmail.com>
2021-04-12 20:59:08 +0900

    refactoring

--- src/pages/owner/MyRestaurantsPage.tsx
+++ src/pages/owner/MyRestaurantsPage.tsx
@@ -10,29 +10,33 @@ function MyRestaurantsPage() {
   const { data } = useQuery<myRestaurantsQuery>(MY_RESTAURANTS_QUERY);
   const onOpenDialog = () => createRestaurantDialogVars(true);
   return (
-    <div className="flex-1 center">
-      <Helmet>
-        <title>Owner Home | Nuber Eats</title>
-      </Helmet>
-      {(data?.myRestaurants.restaurants?.length || 0) > 0 && (
-        <RestaurantGrid restaurants={data?.myRestaurants.restaurants || []} />
-      )}
+    <>
+      <div className={`flex-1`}>
+        <Helmet>
+          <title>Owner Home | Nuber Eats</title>
+        </Helmet>
+        {(data?.myRestaurants.restaurants?.length || 0) > 0 && (
+          <RestaurantGrid restaurants={data?.myRestaurants.restaurants || []} />
+        )}
+      </div>
       {data?.myRestaurants.restaurants?.length === 0 && (
-        <div className="flex justify-center items-center h-full">
-          <div className="mb-wrap text-center">
-            <h1 className="font-semibold text-7xl text-gray-800 mb-5">텅</h1>
-            <h4 className="text-lg font-light">소유한 음식점이 없습니다.</h4>
-            <div
-              className=" mt-3 flex items-center justify-center cursor-pointer"
-              onClick={onOpenDialog}
-            >
-              <span className="link">음식점 등록하기</span>
-              <span className="text-3xl ml-1"> 🏃‍♀️ </span>
+        <div className="flex-1 mt-16">
+          <div className="flex justify-center items-center h-full">
+            <div className="mb-wrap text-center">
+              <h1 className="font-semibold text-7xl text-gray-800 mb-5">텅</h1>
+              <h4 className="text-lg font-light">소유한 음식점이 없습니다.</h4>
+              <div
+                className=" mt-3 flex items-center justify-center cursor-pointer"
+                onClick={onOpenDialog}
+              >
+                <span className="link">음식점 등록하기</span>
+                <span className="text-3xl ml-1"> 🏃‍♀️ </span>
+              </div>
             </div>
           </div>
         </div>
       )}
-    </div>
+    </>
   );
 }
 
